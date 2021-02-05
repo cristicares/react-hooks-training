8632decd39efbfc26158bf2db08b3eaba0434392
Miguel Acevedo Cádiz <macevedoinf@gmail.com>
2021-02-05 00:03:31 -0300

    Fix bad syntax

--- src/ContextAssignment/LayoutContext.js
+++ src/ContextAssignment/LayoutContext.js
@@ -1,39 +1,44 @@
-import { useReducer } from 'react';
+import { useReducer } from "react";
 
 const initialState = {
-    mainColor: "#120078",
-    secondaryColor: "#9d0191",
-    contrastColor: "#FFFFFF",
+  mainColor: "#120078",
+  secondaryColor: "#9d0191",
+  contrastColor: "#FFFFFF",
 };
 
 const reducer = (state, action) => {
-    switch(action.type) {
-        case "CHANGE_MAIN_COLOR": {
-            return {
-                ...state,
-                mainColor: ''//setear el color nuevo
-            }
-        }
-        case "CHANGE_SECONDARY_COLOR": {
-            return {
-                ...state,
-                secondaryColor: ''//setear el color nuevo
-            }
-        }
-        case "CHANGE_CONTRAST_COLOR": {
-            return {
-                ...state,
-                contrastColor: ''//setear el color nuevo
-            }
-        }
+  switch (action.type) {
+    case "CHANGE_MAIN_COLOR": {
+      return {
+        ...state,
+        mainColor: "", //setear el color nuevo
+      };
     }
-}
+    case "CHANGE_SECONDARY_COLOR": {
+      return {
+        ...state,
+        secondaryColor: "", //setear el color nuevo
+      };
+    }
+    case "CHANGE_CONTRAST_COLOR": {
+      return {
+        ...state,
+        contrastColor: "", //setear el color nuevo
+      };
+    }
+    default: {
+      return {
+        ...state,
+      };
+    }
+  }
+};
 
 const LayoutContext = {}; //Crear contexto
 
 const LayoutContextProvider = ({ children }) => {
-    const { state, dispatch } = useReducer(reducer, initialState);
-    return <div>{children}</div>; //Colocar el provider del contexto con state y dispatch en un objeto dentro de la propiedad value
-}
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return <div>{children}</div>; //Colocar el provider del contexto con state y dispatch en un objeto dentro de la propiedad value
+};
 
 export { LayoutContextProvider, LayoutContext };
